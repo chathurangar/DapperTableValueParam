efab1010d6fc7c0875470b2a29fc82a0736b1d89
Chathuranga Ranasinghe <crranasinghe@outlook.com>
2021-10-09 14:28:58 +0530

    Added code

---
+++ DapperBulkInsert/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace DapperBulkInsert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(113, 60);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(353, 83);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 215);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
+            this.Text = "INSERT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+    }
+}
+
---
+++ DapperBulkInsert/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperBulkInsert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Location");
+
+            dt.Rows.Add("Mark", "London");
+            dt.Rows.Add("John", "NY");
+            dt.Rows.Add("James", "Paris");
+
+            using (IDbConnection conn = Connection)
+            {
+                conn.Execute("InsertUsers", new { users = dt.AsTableValuedParameter("UserTableType") }, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
